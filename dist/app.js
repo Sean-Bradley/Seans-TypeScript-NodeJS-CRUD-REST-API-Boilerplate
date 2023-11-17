0542164d3c949cabbde6ac351a37796079082738
Sean Bradley <seanwasere@gmail.com>
2023-11-17 22:45:13 +0000

    updated packages to latest

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
@@ -25,7 +41,7 @@ class App {
                     .on('error', (err) => reject(err));
             });
         };
-        this.httpServer = express_1.default();
+        this.httpServer = (0, express_1.default)();
         this.httpServer.use(bodyParser.urlencoded({ extended: true }));
         this.httpServer.use(bodyParser.json());
         new router_1.default(this.httpServer);
--- dist/router.js
+++ dist/router.js
@@ -1,9 +1,25 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
@@ -17,25 +33,25 @@ class Router {
     constructor(server) {
         const router = express.Router();
         const cats = new Map();
-        cats[uuid_1.v4()] = { genus: "feline", name: "Cosmo", isHungry: true, lastFedDate: new Date() };
-        cats[uuid_1.v4()] = { genus: "feline", name: "Emmy", isHungry: true, lastFedDate: new Date() };
+        cats[(0, uuid_1.v4)()] = { genus: "feline", name: "Cosmo", isHungry: true, lastFedDate: new Date() };
+        cats[(0, uuid_1.v4)()] = { genus: "feline", name: "Emmy", isHungry: true, lastFedDate: new Date() };
         router.get('/', (req, res) => {
             res.json({
                 message: `Nothing to see here, [url]/cats instead.`
             });
         });
         //get all cats
-        router.get('/cats', cors_1.default(), (req, res) => {
+        router.get('/cats', (0, cors_1.default)(), (req, res) => {
             res.json({
                 cats
             });
         });
         //create new cat
-        router.post('/cats', cors_1.default(), (req, res) => {
+        router.post('/cats', (0, cors_1.default)(), (req, res) => {
             try {
                 let cat = {};
                 Object.assign(cat, req.body);
-                const newUUID = uuid_1.v4();
+                const newUUID = (0, uuid_1.v4)();
                 cats[newUUID] = cat;
                 res.json({
                     uuid: newUUID
@@ -46,7 +62,7 @@ class Router {
             }
         });
         //get cat by id
-        router.get('/cats/:id', cors_1.default(), (req, res) => {
+        router.get('/cats/:id', (0, cors_1.default)(), (req, res) => {
             if (!!cats[req.params.id]) {
                 res.json({
                     cat: cats[req.params.id]
@@ -57,7 +73,7 @@ class Router {
             }
         });
         //update cat
-        router.put('/cats/:id', cors_1.default(), (req, res) => {
+        router.put('/cats/:id', (0, cors_1.default)(), (req, res) => {
             try {
                 if (!!cats[req.params.id]) {
                     let cat = {};
@@ -76,7 +92,7 @@ class Router {
             }
         });
         //delete cat
-        router.delete('/cats/:id', cors_1.default(), (req, res) => {
+        router.delete('/cats/:id', (0, cors_1.default)(), (req, res) => {
             if (!!cats[req.params.id]) {
                 delete cats[req.params.id];
                 res.json({
@@ -87,7 +103,7 @@ class Router {
                 res.status(404).send(JSON.stringify({ "error": "no such cat" }));
             }
         });
-        router.options('*', cors_1.default());
+        router.options('*', (0, cors_1.default)());
         server.use('/', router);
     }
 }
